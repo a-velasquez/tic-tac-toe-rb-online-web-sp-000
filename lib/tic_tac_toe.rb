063cae1e42ab6981ed0ff6996f339ac47dba66a3
Alex Velasquez <jtinnesco@gmail.com>
2019-07-23 05:06:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,14 +58,5 @@ WIN_COMBINATIONS = [
     end
     end
 
-    def turn_count(board)
-    counter = 0
-    board.each do |space|
-    if space == "X" || space == "O"
-    counter += 1
-    end
-    end
-    counter
-    end
 
 end
